52414a4905946cdf349e097c2786b99e108a87cd
rocker0516 <fgdhserkhk@gmail.com>
2018-10-21 17:56:38 +0800

    Project init

---
+++ test_p12/src/test_p12/Sample3_5.java
@@ -0,0 +1,32 @@
+package test_p12;
+
+public class Sample3_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1;
+		car1 = new Car();
+		car1.num =1234;
+		car1.gas=20.5;
+		
+		car1.showCar();
+	}
+
+}
+class Car
+{
+	int num;
+	double gas;
+	
+	void show()
+	{
+		System.out.println("車號是"+this.num);
+		System.out.println("汽油量是"+this.gas);
+	}
+	
+	void showCar()
+	{
+		System.out.println("開始顯示車子資料");
+		this.show();
+	}
+}
